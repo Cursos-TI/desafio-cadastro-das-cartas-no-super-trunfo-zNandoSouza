2962a2b6fe1326a79d9a2a88b7c038b0363aadfc
Fernando Souza <160878730+zNandoSouza@users.noreply.github.com>
2025-01-17 23:01:31 +0000

    Novas Propriedades Calculadas (Aventureiro)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,9 @@ int main() {
     int PIB;
     int numeros;
 
+    float densidade;
+    float capita;
+
     printf("Digite o código da cidade: \n");
     scanf(" %d", &codigo);
 
@@ -30,14 +33,21 @@ int main() {
     scanf(" %d", &numeros);
 
 
-printf("Código da cidade: %d \n", codigo);
-printf("Nome da Cidade: %s \n", nome);
-printf("População: %d \n", populacao);
-printf("Área: %f \n", area);
-printf("PIB: %d \n", PIB);
-printf("Números de pontos turísticos %d \n", numeros);
+ printf("Código da cidade: %d \n", codigo);
+ printf("Nome da Cidade: %s \n", nome);
+ printf("População: %d \n", populacao);
+ printf("Área: %f \n", area);
+ printf("PIB: %d \n", PIB);
+ printf("Números de pontos turísticos %d \n", numeros);
+
+ densidade = (populacao / area);
+
+ capita = (PIB / populacao);
+
+  printf("Densidade Populacional: %.2f", densidade);
+
+  printf("PIB per Capita: %.2f", capita);
 
 
-    
     return 0;
 }
